9e31d15b8e04b3d0b2c32157286c9780000171e8
FluffyLinux <fluffywin@gmail.com>
2022-12-05 14:24:52 -0500

    tune up and added features

--- controller.js
+++ controller.js
@@ -6,6 +6,7 @@
 //Send user information to the backend (GET, POST, PUT, DELETE)
 
 class Profile{
+
     constructor(name, age, gender){
         this.name = name;
         this.age = age;
@@ -13,27 +14,62 @@ class Profile{
     }
 }
 
+//made for arrays of objects profile
 let profileList = [];
 
-document.getElementById("profileBox").innerHTML += `
+function loadProfile(){
+    document.getElementById("profileBox").innerHTML = `
+    <div id="profilerCenter"></div>`;
+
+    document.getElementById("profilerCenter").innerHTML += `
     <input type="text" id="name" class="field" placeholder="name"/><br/>
     <input type="text" id="age" class="field" placeholder="age"/><br/>
     <input type="text" id="gender" class="field" placeholder="gender"/><br/>
     <br/>
 `;
 
-
-document.getElementById("profileBox").innerHTML += `
-    <button id="btn" onclick="getInfo()">Submit</button>
+document.getElementById("profilerCenter").innerHTML += `
+    <div>
+        <button id="addbtn" onclick="addInfo()">Add</button>
+        <button id="submitbtn" onclick="sendInfo()">Submit</button>
+    </div>
 `;
+}
 
-function getInfo(){
+
+function addInfo(){
     let name = document.getElementById("name").value;
     let age = document.getElementById("age").value;
     let gender = document.getElementById("gender").value;
 
     profileList.push( new Profile(name, age, gender));
 
+    
     console.log(profileList);
+    sideBarUpdate();
+    loadProfile();
+    
 }
 
+
+function sideBarUpdate(){
+    document.getElementById("sideBar").innerHTML = "";
+    for(let i = 0; i < profileList.length; i++){
+        document.getElementById("sideBar").innerHTML += `
+        <div class="profileAdded">
+          ${profileList[i].name}, ${profileList[i].age}, ${profileList[i].gender}
+        </div>
+    `;
+    }
+}
+
+
+document.getElementById("sideBar").innerHTML += btnTest();
+
+function btnTest(){
+    return "<button>Testing feature</button>";
+}
+
+
+
+loadProfile();
