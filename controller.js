d90f5a4fb3076aa17f9e0d46268f07235af2afa3
FluffyLinux <fluffywin@gmail.com>
2022-12-11 21:02:47 -0500

    implemented edit functionality

--- controller.js
+++ controller.js
@@ -12,6 +12,18 @@ class Profile{
         this.age = age;
         this.gender = gender;
     }
+
+    setName(name){
+        this.name = name;
+    }
+
+    setAge(age){
+        this.age = age;
+    }
+
+    setGender(gender){
+        this.gender = gender;    
+    }
 }
 
 //made for arrays of objects profile
@@ -32,7 +44,7 @@ function loadProfile(){
 document.getElementById("profilerCenter").innerHTML += `
     <div>
         <button id="addbtn" onclick="addInfo()">Add</button>
-        <button id="submitbtn" onclick="deleteProfile()">Remove</button>
+        <button id="submitbtn" onclick="editInterface()">test</button>
     </div>
 `;
 }
@@ -65,7 +77,11 @@ function sideBarUpdate(){
         document.getElementById("sideBar").innerHTML += `
         <div class="profileAdded">
           ${profileList[i].name}, ${profileList[i].age}, ${profileList[i].gender}
-          <button onclick="deleteProfile(${i})">X</button>
+          <div>
+          <button class="sidebtn" onclick="deleteProfile(${i})">X</button>
+          <button class="sidebtn" onclick="editInterface(${i})">edit</button>
+          </div>
+          
         </div>
     `;
     }
@@ -82,6 +98,61 @@ function deleteProfile(target){
 //     return "<button>Testing feature</button>";
 // }
 
+//---------------------------Edit Function Below-----------------------------------------------
+
+function editInterface(target){
+    let name = profileList[target].name;
+    let age = profileList[target].age;
+    let gender = profileList[target].gender;
+
+    document.getElementById("profileBox").innerHTML = `
+        <div id="editBox"></div>
+    `;
+
+    document.getElementById("editBox").innerHTML += `
+        <h1>Edit</h1>
+        <input type="text" id="name" class="field" value="${name}"/><br/>
+        <input type="number" id="age" class="field" value="${age}"/><br/>
+        <input type="text" id="gender" class="field" value="${gender}"/><br/>
+        <br/>
+    `;
+
+    document.getElementById("editBox").innerHTML += `
+        <button onclick="loadProfile()">Cancel</button>
+        <button onclick="updateProfile(${target})">update</button>
+    `;
+    
+}
+
+function updateProfile(target){
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    let gender = document.getElementById("gender").value;
+
+    profileList[target].setName(name);
+    profileList[target].setAge(age);
+    profileList[target].setGender(gender);
+
+    sideBarUpdate();
+    loadProfile();
+}
 
 
-loadProfile();
+//---------------------navBar--------------------
+
+function loadNav(){
+    document.getElementById("navBar").innerHTML += `
+        <div class="navItem" onclick="home()">Home</div>
+        <div class="navItem" onclick="loadProfile()">Ticket</div>
+        <div class="navItem" onclick="">History</div>
+        <div class="navItem" onclick="">Account</div>
+    `;
+}
+
+function loadHome(){
+    document.getElementById("profileBox").innerHTML = `
+        <div id="welcomeMsg">Mochi was here!</div>
+    `;
+}
+loadHome();
+loadNav();
